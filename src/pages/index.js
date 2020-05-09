5ca919e62bfaae6f77fdb5b32823221eaae5e4cd
colinmorris <colin.morris2@gmail.com>
2020-05-08 22:27:49 -0400

    remove some starter cruft

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { graphql } from "gatsby";
 
 import Layout from "../components/layout"
 import PageListing from '~components/pageListing';
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
